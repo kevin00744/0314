2dc6821540cd3372afb5da6a381dcc96579044e0
kevin00744 <52673800+kevin00744@users.noreply.github.com>
2023-03-15 11:55:32 +0800

    Add files via upload

--- hw1.py
+++ hw1.py
@@ -3,14 +3,15 @@
 # • 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, …
 # • Hint: Using recursive equation: F(n)=F(n-1)+F(n-2)
 # with initial conditions F(1)=1 and F(2)=1.
-def Fibonacci(n):
+def Fibonacci(n=1):
     if n == 1 or n == 2:
         return 1
     else:
         return Fibonacci(n-1) + Fibonacci(n-2)
-n = int(input('Input how many number you want to see\n>'))
-for i in range (1,n):
-    print ('%s,' % Fibonacci(i),  sep = "", end = "")
-print (Fibonacci(n))
-
+def print_Fibonacci(n=1):
+    n = int(input('Input how many number you want to see\n>'))
+    for i in range (1,n):
+        print ('%s,' % Fibonacci(i),  sep = "", end = "")
+    print (Fibonacci(n))
+print_Fibonacci()
 
--- hw2.py
+++ hw2.py
@@ -3,13 +3,18 @@
 # • Hint: Find factors of the smaller of x and y, and then
 # check if the factor is also a factor of the larger of x
 # and y. 
-def HCF(x, y):
+def calculate(x,y):
     if x == y:
-         return x 
+        return x 
     elif x > y :
-        return HCF(x-y,y)
+        return calculate(x-y, y)
     else:
-        return HCF(x, y-x)
-x = int(input('Please enter first number\n>'))
-y = int(input('Please enter second number\n>'))
-print ('The %s and %s larger common factor is %s'%(x, y, HCF(x, y)))
+        return calculate(x, y-x)
+def HCF(x=0, y=0):
+    x = int(input('Please enter first number\n>'))
+    y = int(input('Please enter second number\n>'))
+    if x == 0 and y == 0 :
+        print('Is undefinded')
+    else:
+        print ('The %s and %s larger common factor is %s'%(x, y, calculate(x, y)))
+HCF()
